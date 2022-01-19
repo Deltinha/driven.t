fddb71bdcab691ee0e1e2f663450b6975b02ab09
Samuel Felipe <samuelfelipef@gmail.com>
2022-01-19 20:16:40 -0300

    created not enrollment message in payment area

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+import useApi from "../../../hooks/useApi";
+import styled from "styled-components";
+import { Typography } from "@material-ui/core";
+import NotEnrollment from "../../../components/Payment/NotEnrollment";
+
 export default function Payment() {
-  return "Pagamento: Em breve!";
+  const [enrollmentInfo, setEnrollmentInfo] = useState("");
+  const { enrollment } = useApi();
+
+  useEffect(() => {
+    enrollment.getPersonalInformations()
+      .then(res => setEnrollmentInfo(res.data));
+  }, []);
+
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+
+      {!enrollmentInfo ? 
+        <NotEnrollment /> : "lala"  
+      }
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
