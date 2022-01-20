ba0cc65e74e4ec7e47d5ff18a04846e76d716a8e
Moisés Brandão <moisesbrandao1998@gmail.com>
2022-01-20 15:18:12 -0300

    Created "ticket does not includes hotel" screen

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Typography } from "@material-ui/core";
+import useApi from "../../../hooks/useApi";
+
 export default function Hotel() {
-  return "Hotel: Em breve!";
+  const [ticketInfo, setTicketInfo] = useState([]);
+  const { ticket } = useApi();
+
+  useEffect(() => {
+    ticket.getTicketFromUser()
+      .then(res => 
+        setTicketInfo(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
+      {
+        ticketInfo.hasHotel ?
+          <span>SELECT HOTEL</span> 
+          : <span>Sua modalidade não aceita hotel</span>
+      }
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
