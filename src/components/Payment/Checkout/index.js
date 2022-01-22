966b5bc4afed13a3458c6815afd443b28b00d437
Luis Felipe <l_felipelizeu@hotmail.com>
2022-01-22 00:38:31 -0300

    Now if the ticket is paid, it will show a confirmaton message

--- src/components/Payment/Checkout/index.js
+++ src/components/Payment/Checkout/index.js
@@ -1,33 +1,11 @@
 import { useState } from "react";
-import PaymentForm from "./PaymentForm";
-import Button from "../../Form/Button";
-import validations from "./FormValidations";
 import InfoText from "./InfoText";
 import TicketResume from "./TicketResume";
+import Payment from "./Payment";
+import PaidConfirmation from "./PaidConfirmation";
 
 export default function Checkout() {
   const [ticket, setTicket] = useState({});
-  const [number, setNumber] = useState("");
-  const [name, setName] = useState("");
-  const [expiry, setExpiry] = useState("");
-  const [cvc, setCvc] = useState("");
-  const [errors, setErrors] = useState({});
-
-  function submitPayment(event) {
-    event.preventDefault();
-
-    const verifyErrors = {
-      number: validations.number.isValid(number) ? null : validations.number.message,
-      issuer: validations.issuer.isValid(number) ? null : validations.issuer.message,
-      name: validations.name.isValid(name) ? null : validations.name.message,
-      month: validations.expiry.month.isValid(expiry) ? null : validations.expiry.month.message,
-      expired: validations.expiry.expired.isValid(expiry) ? null : validations.expiry.expired.message,
-      cvc: validations.cvc.isValid(cvc) ? null : validations.cvc.message,
-    };
-    setErrors(verifyErrors);
-
-    if (Object.values(verifyErrors).some((error) => error)) return;
-  }
 
   return (
     <>
@@ -36,22 +14,11 @@ export default function Checkout() {
         setTicketInfo={setTicket}
       />
       <InfoText>Pagamento</InfoText>
-      <form onSubmit={(event) => submitPayment(event)}>
-        <PaymentForm
-          number={number}
-          setNumber={setNumber}
-          name={name}
-          setName={setName}
-          expiry={expiry}
-          setExpiry={setExpiry}
-          cvc={cvc}
-          setCvc={setCvc}
-          errors={errors}
-        />
-        <Button type="submit">
-          Finalizar Pagamento
-        </Button>
-      </form>
+      {ticket?.isPaid ? (
+        <PaidConfirmation />
+      ) : (
+        <Payment />
+      )}
     </>
   );
 }
