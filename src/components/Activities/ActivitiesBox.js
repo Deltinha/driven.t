0b4187b7b8d276f0afc5f6c604b3a7e93061bed1
Luis Felipe <l_felipelizeu@hotmail.com>
2022-01-28 00:31:49 -0300

    Now the scroll of the board is the same for all locals

--- src/components/Activities/ActivitiesBox.js
+++ src/components/Activities/ActivitiesBox.js
@@ -3,14 +3,6 @@ import styled from "styled-components";
 const ActivitiesBox = styled.div`
     width: 100%;
     height: 70%;
-    display: grid;
-    grid-template-columns: repeat(3 , 33.33%);
-
-    & div:first-child {
-        ul {
-            border-left: 1px solid #d7d7d7;
-        }
-    }
 `;
 
 export default ActivitiesBox;
