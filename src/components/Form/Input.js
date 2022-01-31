6a443e37cabd962dd189b00e4e262583231bd93d
Samuel Felipe <samuelfelipef@gmail.com>
2022-01-31 16:38:54 -0300

    update hotel page title on mobile

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -3,15 +3,6 @@ import styled from "styled-components";
 import InputMask from "react-input-mask";
 
 export default function Input({ mask = "", maskChar = "", formatChars, variant = "outlined", value="", onChange = () => 0, ...props }) {
-/*   console.log("***", {
-    mask,
-    maskChar,
-    formatChars,
-    variant,
-    value,
-    onChange,
-    ...props
-  }); */
   return (mask || maskChar) ? (
     <InputMask  mask={mask} maskChar={maskChar} value={value} onChange={onChange} {...(formatChars && { formatChars })}>
       {() => <StyledTextField {...props} variant={variant} />}
